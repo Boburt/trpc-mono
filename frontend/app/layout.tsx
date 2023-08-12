4adddb4952c3fb90607a175bc125ae2994e3cd64
Davr <davr@Davrs-NewiMac.local>
2023-08-12 15:28:11 +0500

    ss

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,6 +7,7 @@ import { Search } from "lucide-react";
 import { UserNav } from "@frontend/components/layout/user-nav";
 import { ModeToggle } from "@frontend/components/layout/mode-toggle";
 import { Toaster } from "@components/ui/toaster";
+import { Providers } from "@frontend/store/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,20 +26,22 @@ export default function RootLayout({
       <head />
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex-col">
-            <div className="border-b">
-              <div className="flex h-16 items-center px-4">
-                <NavigationMenuDemo />
-                <div className="ml-auto flex items-center space-x-4">
-                  <Search />
-                  <UserNav />
-                  <ModeToggle />
+          <Providers>
+            <div className="flex-col">
+              <div className="border-b">
+                <div className="flex h-16 items-center px-4">
+                  <NavigationMenuDemo />
+                  <div className="ml-auto flex items-center space-x-4">
+                    <Search />
+                    <UserNav />
+                    <ModeToggle />
+                  </div>
                 </div>
               </div>
+              <div className="mx-4 mt-10 mb-4">{children}</div>
             </div>
-            <div className="mx-4 mt-10 mb-4">{children}</div>
-          </div>
-          <Toaster />
+            <Toaster />
+          </Providers>
         </ThemeProvider>
       </body>
     </html>
