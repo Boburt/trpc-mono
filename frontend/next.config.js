0dc17ca62e3d8ece88b96f61113acf7239654b47
Davr <davr@Davrs-NewiMac.local>
2023-08-14 09:58:18 +0500

    trpc-rtk is addedd

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,3 +14,27 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// import million from "million/compiler";
+// const nextConfig = {
+//   experimental: {
+//     swcPlugins: [
+//       [
+//         "next-superjson-plugin",
+//         {
+//           excluded: [],
+//         },
+//       ],
+//     ],
+//     externalDir: true,
+//   },
+//   reactStrictMode: true,
+// };
+
+// const millionConfig = {
+//   auto: true,
+//   // if you're using RSC:
+//   // auto: { rsc: true },
+// };
+
+// export default million.next(nextConfig, millionConfig);
