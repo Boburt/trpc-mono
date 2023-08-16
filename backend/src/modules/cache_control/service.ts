3245606bde9bde6025dbe594fcf71ccadaf18ccc
Bobur <boburmac@Boburs-iMac-Pro.local>
2023-08-16 14:22:45 +0500

    dsgfsklgjsf

--- backend/src/modules/cache_control/service.ts
+++ backend/src/modules/cache_control/service.ts
@@ -1,5 +1,5 @@
 import { DB, RedisClientType } from "@backend/trpc";
-import { permissions } from "@prisma/client";
+import { organization, permissions, roles } from "@prisma/client";
 
 export class CacheControlService {
   constructor(
@@ -7,6 +7,7 @@ export class CacheControlService {
     private readonly redis: RedisClientType
   ) {
     this.cachePermissions();
+    this.cacheOrganization();
   }
 
   async cachePermissions() {
@@ -26,6 +27,50 @@ export class CacheControlService {
       `${process.env.PROJECT_PREFIX}permissions`
     );
     let res = JSON.parse(permissions ?? "[]");
+
+    if (take && res.length > take) {
+      res = res.slice(0, take);
+    }
+
+    return res;
+  }
+
+  async cacheOrganization() {
+    const organization = await this.prisma.organization.findMany();
+    await this.redis.set(
+      `${process.env.PROJECT_PREFIX}organization`,
+      JSON.stringify(organization)
+    );
+  }
+
+  async getCachedOrganization({
+    take,
+  }: {
+    take?: number;
+  }): Promise<organization[]> {
+    const organization = await this.redis.get(
+      `${process.env.PROJECT_PREFIX}organization`
+    );
+    let res = JSON.parse(organization ?? "[]");
+
+    if (take && res.length > take) {
+      res = res.slice(0, take);
+    }
+
+    return res;
+  }
+
+  async cacheRoles() {
+    const roles = await this.prisma.roles.findMany();
+    await this.redis.set(
+      `${process.env.PROJECT_PREFIX}roles`,
+      JSON.stringify(roles)
+    );
+  }
+
+  async getCachedRoles({ take }: { take?: number }): Promise<roles[]> {
+    const roles = await this.redis.get(`${process.env.PROJECT_PREFIX}roles`);
+    let res = JSON.parse(roles ?? "[]");
 
     if (take && res.length > take) {
       res = res.slice(0, take);
