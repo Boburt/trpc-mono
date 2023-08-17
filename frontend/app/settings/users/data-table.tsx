b7aa659f494b907f02d1a7a185084fe64c40e5cc
Davr <davr@Davrs-NewiMac.local>
2023-08-17 14:08:34 +0500

    saving local changes

--- frontend/app/settings/users/data-table.tsx
+++ frontend/app/settings/users/data-table.tsx
@@ -2,8 +2,10 @@
 
 import {
   ColumnDef,
+  PaginationState,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -18,7 +20,8 @@ import {
 
 import { Button } from "@components/ui/button";
 
-import { useMemo } from "react";
+import { useUsersQuery } from "@frontend/store/api/users";
+import { useMemo, useState } from "react";
 import {
   Select,
   SelectContent,
@@ -32,45 +35,45 @@ import {
   DoubleArrowLeftIcon,
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
-import { useRolesStore } from "@frontend/store/states/roles";
-import { trpc } from "@frontend/utils/trpc";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
 }
 
-export function RolesPermissionsDataTable<TData, TValue>({
+export function DataTable<TData, TValue>({
   columns,
 }: DataTableProps<TData, TValue>) {
-  const rowSelection = useRolesStore((state) => state.selectedRows);
+  const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
 
-  const selectedRoleId = useMemo(() => {
-    return Object.keys(rowSelection)[0];
-  }, [rowSelection]);
-
-  const { data, isLoading } = trpc.rolesPermissions.list.useQuery(
-    {
-      where: {
-        role_id: {
-          equals: selectedRoleId,
-        },
-      },
-    },
-    {
-      enabled: Object.keys(rowSelection).length > 0,
-    }
-  );
+  const { data, isLoading } = useUsersQuery({
+    take: pageSize,
+    skip: pageIndex * pageSize,
+  });
 
   const defaultData = useMemo(() => [], []);
-  const table = useReactTable({
-    data: data ?? defaultData,
-    columns,
-    // onRowSelectionChange: function (stateUpdater) {
-    //   console.log(arguments);
-    //   // setRowSelection(stateUpdater)
-    // },
 
+  const pagination = useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize]
+  );
+
+  const table = useReactTable({
+    data: data?.items ?? defaultData,
+    columns,
+    pageCount: data?.meta?.pageCount ?? -1,
+    state: {
+      pagination,
+    },
+    onPaginationChange: setPagination,
     getCoreRowModel: getCoreRowModel(),
+    manualPagination: true,
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
@@ -96,7 +99,7 @@ export function RolesPermissionsDataTable<TData, TValue>({
             ))}
           </TableHeader>
           <TableBody>
-            {isLoading && selectedRoleId ? (
+            {isLoading ? (
               <TableRow>
                 <TableCell
                   colSpan={columns.length}
@@ -154,6 +157,76 @@ export function RolesPermissionsDataTable<TData, TValue>({
             )}
           </TableBody>
         </Table>
+      </div>
+      <div className="h-2" />
+      <div className="flex items-center justify-between px-2">
+        <div className="flex-1 text-sm text-muted-foreground"></div>
+        <div className="flex items-center space-x-6 lg:space-x-8">
+          <div className="flex items-center space-x-2">
+            <p className="text-sm font-medium">Rows per page</p>
+            <Select
+              value={`${table.getState().pagination.pageSize}`}
+              onValueChange={(value) => {
+                table.setPageSize(Number(value));
+              }}
+            >
+              <SelectTrigger className="h-8 w-[70px]">
+                <SelectValue
+                  placeholder={table.getState().pagination.pageSize}
+                />
+              </SelectTrigger>
+              <SelectContent side="top">
+                {[10, 20, 30, 40, 50].map((pageSize) => (
+                  <SelectItem key={pageSize} value={`${pageSize}`}>
+                    {pageSize}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          Page {table.getState().pagination.pageIndex + 1} of{" "}
+          {table.getPageCount()}
+        </div>
+        <div className="flex items-center space-x-2">
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to first page</span>
+            <DoubleArrowLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to previous page</span>
+            <ChevronLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to next page</span>
+            <ChevronRightIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to last page</span>
+            <DoubleArrowRightIcon className="h-4 w-4" />
+          </Button>
+        </div>
       </div>
     </div>
   );
---
+++ frontend/store/api/users.ts
@@ -0,0 +1,29 @@
+import { trpc, ReactQueryOptions } from "@frontend/utils/trpc";
+
+export function useUsersQuery(filter: any) {
+  return trpc.users.list.useQuery(filter);
+}
+
+export function useUsersCreate(options: ReactQueryOptions["users"]["add"]) {
+  const utils = trpc.useContext();
+  return trpc.users.add.useMutation({
+    ...options,
+    onSuccess: (post) => {
+      utils.users.list.invalidate();
+
+      options?.onSuccess?.(post);
+    },
+  });
+}
+
+export function useUsersUpdate(options: ReactQueryOptions["users"]["renew"]) {
+  const utils = trpc.useContext();
+  return trpc.users.renew.useMutation({
+    ...options,
+    onSuccess: (post) => {
+      utils.users.list.invalidate();
+
+      options?.onSuccess?.(post);
+    },
+  });
+}
