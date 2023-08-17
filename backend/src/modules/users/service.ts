b9462661139430ebb2cc02d5040c0addf734249f
Bobur <boburmac@Boburs-iMac-Pro.local>
2023-08-17 16:53:45 +0500

    excluding password

--- backend/src/modules/users/service.ts
+++ backend/src/modules/users/service.ts
@@ -2,7 +2,7 @@ import { PaginationType } from "@backend/lib/pagination_interface";
 import {
   usersFindManyArgsSchema,
   usersFindUniqueArgsSchema,
-  users,
+  users as usersSchema,
 } from "@backend/lib/zod";
 import { DB } from "@backend/trpc";
 import { Prisma } from "@prisma/client";
@@ -11,13 +11,13 @@ import { z } from "zod";
 export class UsersService {
   constructor(private readonly prisma: DB) {}
 
-  async create(input: Prisma.usersCreateArgs): Promise<users> {
+  async create(input: Prisma.usersCreateArgs): Promise<usersSchema> {
     return await this.prisma.users.create(input);
   }
 
   async findMany(
     input: z.infer<typeof usersFindManyArgsSchema>
-  ): Promise<PaginationType<users>> {
+  ): Promise<PaginationType<Omit<usersSchema, "password">>> {
     let take = input.take ?? 20;
     let skip = !input.skip ? 1 : Math.round(input.skip / take);
     if (input.skip && input.skip > 0) {
@@ -30,21 +30,47 @@ export class UsersService {
       page: skip,
       includePageCount: true,
     });
-    return { items: users, meta };
+    return {
+      items: users.map((user) => {
+        return this.exclude(user, ["password"]) as Omit<
+          usersSchema,
+          "password"
+        >;
+      }),
+      meta,
+    };
   }
 
   async findOne(
     input: z.infer<typeof usersFindUniqueArgsSchema>
-  ): Promise<users | null> {
-    const res = await this.prisma.users.findUnique(input);
-    return res;
+  ): Promise<Omit<usersSchema, "password"> | null> {
+    const user = await this.prisma.users.findUnique(input);
+
+    if (!user) {
+      return null;
+    }
+
+    return this.exclude(user, ["password"]) as Omit<usersSchema, "password">;
   }
 
-  async update(input: Prisma.usersUpdateArgs): Promise<users> {
+  private exclude<User extends Record<string, unknown>, Key extends keyof User>(
+    user: User,
+    keys: Key[]
+  ): Omit<User, Key> {
+    const filteredEntries = Object.entries(
+      user as Record<string, unknown>
+    ).filter(([key]) => keys.includes(key as Key));
+    const filteredObject = Object.fromEntries(
+      filteredEntries
+    ) as unknown as Omit<User, Key>;
+    return filteredObject;
+  }
+
+  async update(input: Prisma.usersUpdateArgs): Promise<usersSchema> {
     return await this.prisma.users.update(input);
   }
 
-  async delete(input: Prisma.usersDeleteArgs): Promise<users> {
+  async delete(input: Prisma.usersDeleteArgs): Promise<usersSchema> {
     return await this.prisma.users.delete(input);
   }
 }
