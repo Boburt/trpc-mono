977e670ec098bad6c3a3f72e4e3cb4dbf0089082
ilhombek2110 <ilhombek2110@gmail.com>
2023-08-23 10:18:41 +0500

    11

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
 /* eslint-disable react/jsx-no-comment-textnodes */
 import Manufacturer11 from "@/components/manufacturer/Manufacturer";
 import Image from "next/image";
 import Link from "next/link";
-=======
 import Filter from "@/components/Filter";
->>>>>>> e567bf3fe5bd71f386d42b742dee31ab94a9a86e
 
 export default function Home() {
   return (
     <main>
-<<<<<<< HEAD
-      Body
       <Manufacturer11 />
-=======
       <Filter />
->>>>>>> e567bf3fe5bd71f386d42b742dee31ab94a9a86e
     </main>
   );
 }
