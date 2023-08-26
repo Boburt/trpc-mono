5bdd0d4bcaefbaf8981330f0729494a9494ddec8
Davr <davr@Davrs-NewiMac.local>
2023-08-26 20:06:56 +0500

    langs and categories

---
+++ admin/app/system/langs/columns.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Edit2Icon } from "lucide-react";
+import { Button } from "@components/ui/button";
+import { RouterOutputs } from "@admin/utils/trpc";
+import LangsFormSheet from "@admin/components/forms/langs/sheet";
+import DeleteAction from "./delete-action";
+import { Switch } from "@components/ui/switch";
+
+export const langsColumns: ColumnDef<
+  RouterOutputs["langs"]["list"]["items"][0]
+>[] = [
+  {
+    accessorKey: "is_default",
+    header: "По умолчанию",
+    cell: ({ row }) => {
+      const record = row.original;
+
+      return (
+        <div className="flex items-center space-x-2">
+          <Switch checked={record.is_default} disabled />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "code",
+    header: "Код",
+  },
+  {
+    accessorKey: "name",
+    header: "Заголовок",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const record = row.original;
+
+      return (
+        <div className="flex items-center space-x-2">
+          <LangsFormSheet recordId={record.id}>
+            <Button variant="outline" size="sm">
+              <Edit2Icon className="h-4 w-4" />
+            </Button>
+          </LangsFormSheet>
+          <DeleteAction recordId={record.id} />
+        </div>
+      );
+    },
+  },
+];
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,35 +13,11 @@ const app = new Elysia()
   .use(
     trpc(router, {
       createContext,
-      // responseMeta: (opts) => {
-      //   const { ctx, paths, errors, type } = opts;
-      //   // assuming you have all your public routes with the keyword `public` in them
-      //   const allPublic =
-      //     paths && paths.every((path: string) => path.includes("public"));
-      //   // checking that no procedures errored
-      //   const allOk = errors.length === 0;
-      //   // checking we're doing a query request
-      //   const isQuery = type === "query";
-      //   if (ctx?.res && allPublic && allOk && isQuery) {
-      //     // cache request for 1 day + revalidate once every second
-      //     const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
-      //     return {
-      //       headers: {
-      //         "cache-control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
-      //       },
-      //     };
-      //   }
-      //   return {};
-      // },
     })
   )
   .listen(3000);
 
 export type App = typeof app;
-
-app.onStop(() => {
-  console.log("🦊 Elysia is stopping...");
-});
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
