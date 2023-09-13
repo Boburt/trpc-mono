5c930dd07a0c42c8256f1432c54a119b403a7f7b
ilhombek2110 <ilhombek2110@gmail.com>
2023-09-13 11:53:12 +0500

    1309

--- b2b/components/Map/Map.tsx
+++ b2b/components/Map/Map.tsx
@@ -1,44 +1,26 @@
-import { useEffect, useRef, useState } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+"use client";
+import React from "react";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
-function Map({ address }: { address: string }) {
-  const mapRef = useRef(null);
-  const [googleMaps, setGoogleMaps] = useState(null);
+const MapYandex = () => (
+  <YMaps
+    query={{
+      ns: "use-load-option",
+      load: "Map,Placemark,control.ZoomControl,control.FullscreenControl,geoObject.addon.balloon",
+    }}
+  >
+    <Map
+      defaultState={{
+        center: [41.311158, 69.279737],
+        zoom: 15,
+        controls: ["zoomControl", "fullscreenControl"],
+      }}
+      width="100%"
+      height="500px"
+    >
+      <Placemark defaultGeometry={[41.311158, 69.279737]} />
+    </Map>
+  </YMaps>
+);
 
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-      version: "weekly",
-    });
-
-    loader.load().then(() => {
-      setGoogleMaps(window.google); // Устанавливаем объект google
-    });
-  }, []);
-
-  useEffect(() => {
-    if (googleMaps) {
-      const geocoder = new googleMaps.maps.Geocoder(); // Используем googleMaps
-      geocoder.geocode({ address: address }, (results, status) => {
-        if (status === "OK") {
-          const map = new googleMaps.maps.Map(mapRef.current, {
-            center: results[0].geometry.location,
-            zoom: 8,
-          });
-          const marker = new googleMaps.maps.Marker({
-            map: map,
-            position: results[0].geometry.location,
-          });
-        } else {
-          console.error(
-            `Geocode was not successful for the following reason: ${status}`
-          );
-        }
-      });
-    }
-  }, [address, googleMaps]);
-
-  return <div style={{ height: "400px" }} ref={mapRef} />;
-}
-
-export default Map;
+export default MapYandex;
