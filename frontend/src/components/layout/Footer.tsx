5df8ba3e2f65815e0ee854f7d10f11a44926a2c0
Bobur <bobur@Boburs-iMac-Pro.local>
2023-10-04 10:17:03 -0700

    static pages

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -11,14 +11,18 @@ export default function Footer() {
         </div>
         <div>
           <span className="footer-title">Company</span>
-          <a className="link link-hover">About us</a>
+          <a className="link link-hover" href="/about">
+            About us
+          </a>
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Jobs</a>
           <a className="link link-hover">Press kit</a>
         </div>
         <div>
           <span className="footer-title">Legal</span>
-          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover" href="terms">
+            Terms of use
+          </a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </div>
