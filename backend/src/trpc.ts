754d2d422ae37dfae5d98852249d23dbe384026a
Davr <davr@Davrs-NewiMac.local>
2023-10-13 22:09:04 +0500

    facet filters done

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -27,12 +27,11 @@ import { CitiesService } from "./modules/cities/service";
 import { ManufacturersPropertiesCategoriesService } from "./modules/manufacturers_properties_categories/service";
 import { ManufacturersPropertiesService } from "./modules/manufacturers_properties/service";
 
-
 // redis
 export const client = new Redis({
-port: 6379, // Redis port
+  port: 6379, // Redis port
   host: "127.0.0.1", // Redis host
-//   maxRetriesPerRequest: null,
+  //   maxRetriesPerRequest: null,
 });
 export type RedisClientType = typeof client;
 
