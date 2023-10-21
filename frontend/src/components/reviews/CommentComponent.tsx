c39e785b62c1ed0fdfa1529c658b4528de7598cd
Davr <davr@Davrs-NewiMac.local>
2023-10-21 17:06:16 +0500

    working

--- frontend/src/components/reviews/CommentComponent.tsx
+++ frontend/src/components/reviews/CommentComponent.tsx
@@ -4,7 +4,7 @@ import { signIn } from "auth-astro/client";
 const CommentComponent = () => {
   return (
     <section className="py-8 lg:py-16 antialiased">
-      <button onClick={signIn("credentials")}>Davr</button>
+      <button onClick={() => signIn("credentials")}>Davr</button>
       <div className="max-w-2xl mx-auto px-4">
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
