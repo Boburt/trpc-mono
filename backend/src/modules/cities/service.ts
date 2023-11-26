82832efd0a715ab54d8f7f92413444c582ef4b05
i7671757 <ilhombek2110@gmail.com>
2023-11-26 20:25:20 +0500

    langs

--- backend/src/modules/cities/service.ts
+++ backend/src/modules/cities/service.ts
@@ -9,8 +9,9 @@ import {
 import { PaginationType } from "@backend/lib/pagination_interface";
 import { CacheControlService } from "../cache_control/service";
 import { DB } from "@backend/db";
-import {DrizzleDB} from "@backend/lib/db";
-import {cities} from "@backend/drizzle/schema";
+import { DrizzleDB } from "@backend/lib/db";
+import { cities } from "@backend/../drizzle/schema";
+import { InferSelectModel, sql } from "drizzle-orm";
 
 export class CitiesService {
   constructor(
@@ -27,28 +28,59 @@ export class CitiesService {
 
   async findMany(
     input: z.infer<typeof CitiesFindManyArgsSchema>
-  ): Promise<PaginationType<Cities>> {
+  ): Promise<PaginationType<InferSelectModel<typeof cities>>> {
     let take = input.take ?? 20;
-    let skip = !input.skip ? 1 : Math.round(input.skip / take);
-    if (input.skip && input.skip > 0) {
-      skip++;
-    }
+    let skip = input.skip ?? 0;
     delete input.take;
     delete input.skip;
 
+    const citiesCount = await this.drizzle
+      .select({ count: sql<number>`count(*)` })
+      .from(cities)
+      .execute();
+
+    const citiesList = await this.drizzle
+      .select()
+      .from(cities)
+      .limit(take)
+      .offset(skip);
+
+    /*
+    create new object variable called paginationMeta that will contain data like this:
+        {
+          "isFirstPage": true,
+          "isLastPage": false,
+          "currentPage": 1,
+          "previousPage": null,
+          "nextPage": 2,
+          "pageCount": 2,
+          "totalCount": 14
+      } 
+    */
+    const isLastPage = skip + take >= +citiesCount[0].count;
+    const paginationMeta = {
+      isFirstPage: skip === 0,
+      isLastPage,
+      currentPage: skip == 0 ? 1 : skip / take + 1,
+      previousPage: skip == 0 ? 0 : skip / take,
+      nextPage: skip == 0 ? 2 : isLastPage ? skip / take + 1 : skip / take + 2,
+      pageCount: Math.ceil(+citiesCount[0].count / take),
+      totalCount: +citiesCount[0].count,
+    };
+
     // const permissions = await this.drizzle.select().from(cities);
 
-    const [permissions, meta] = await this.prisma.cities
-      .paginate(input)
-      .withPages({
-        limit: take,
-        page: skip,
-        includePageCount: true,
-      });
+    // const [permissions, meta] = await this.prisma.cities
+    //   .paginate(input)
+    //   .withPages({
+    //     limit: take,
+    //     page: skip,
+    //     includePageCount: true,
+    //   });
 
     return {
-      items: permissions,
-      meta,
+      items: citiesList,
+      meta: paginationMeta,
     };
   }
 
