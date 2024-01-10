7b4fea98230ef90152f089eddd0588f7956cb0c8
Ilhombek <ilhombek2110@gmail.com>
2024-01-10 10:44:55 +0000

    commit -m "form2"

--- frontend/src/components/profile/ProfileRew.tsx
+++
@@ -1,7 +0,0 @@
-export const ProfileRew = () => {
-  return (
-    <div>
-      <h1>ProfileRew</h1>
-    </div>
-  );
-};
--- frontend/src/components/profile/user/Osnavnoy.tsx
+++
@@ -1,7 +0,0 @@
-export const Osnavnoy = () => {
-  return (
-    <div>
-      <h1>Osnavnoy</h1>
-    </div>
-  );
-};
