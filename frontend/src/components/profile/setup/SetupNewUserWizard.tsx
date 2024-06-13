20d2bbc317dfe810d45f1a15b4a44e41cbf344d1
Bobur Juraev <bobur@Boburs-iMac.local>
2024-06-13 10:18:08 +0500

    dslfhjkdffjkf

--- frontend/src/components/profile/setup/SetupNewUserWizard.tsx
+++ frontend/src/components/profile/setup/SetupNewUserWizard.tsx
@@ -8,11 +8,15 @@ import { IoIosArrowForward } from "react-icons/io";
 import { useForm } from "react-hook-form";
 import { VscOrganization, VscPerson } from "react-icons/vsc";
 
-type FirstFormData = {
+type SecondFormData = {
   brandName: string;
   companyName: string;
   companyCategory: string;
   companyCity: string;
+};
+
+type FirstFormData = {
+  roleName: string;
 };
 
 const FirstStepForm = () => {
@@ -21,6 +25,46 @@ const FirstStepForm = () => {
     formState: { errors },
     handleSubmit,
   } = useForm<FirstFormData>();
+  const onSubmit = handleSubmit((data) => $nextStep());
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="grid h-screen w-full">
+        <div className="flex justify-center items-center">
+          <div className="max-w-md">
+            <div className="flex space-x-5 justify-around mt-5">
+              <a
+                href="/sign-up/existing"
+                className="w-44 aspect-square shadow-lg rounded-xl bg-primary flex flex-col space-y-3 justify-center items-center text-white font-bold text-lg cursor-pointer hover:bg-opacity-80 transition-colors duration-300 ease-in-out"
+              >
+                <span>
+                  <VscOrganization size="4rem" />
+                </span>
+                <span>Существующей</span>
+              </a>
+
+              <a
+                href="/sign-up/new"
+                className="w-44 aspect-square shadow-lg rounded-xl bg-primary flex flex-col space-y-3 justify-center items-center text-white font-bold text-lg cursor-pointer hover:bg-opacity-80 transition-colors duration-300 ease-in-out"
+              >
+                <span>
+                  <VscPerson size="4rem" />
+                </span>
+                <span> Новой</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+const SecondStepForm = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<SecondFormData>();
   const onSubmit = handleSubmit((data) => $nextStep());
   return (
     <form onSubmit={onSubmit}>
@@ -74,7 +118,7 @@ const FirstStepForm = () => {
   );
 };
 
-const SecondStepForm = () => {
+const ThirdStepForm = () => {
   return <div>Second Screen</div>;
 };
 
@@ -83,63 +127,64 @@ const StepForms: Record<number, () => JSX.Element> = {
   2: SecondStepForm,
 };
 
-export const SignNewManufacturerWizard = () => {
+export const SetupNewUserWizard = () => {
   const currentStep = useStore($currentStep);
   const steps = [
     {
       number: 1,
-      text: "Компания",
+      text: "Выбор роли",
     },
     {
       number: 2,
-      text: "Профиль",
+      text: "Компания",
     },
     {
       number: 3,
+      text: "Профиль",
+    },
+    {
+      number: 4,
       text: "Итого",
     },
   ];
   const CurrentForm = StepForms[currentStep];
   return (
-    <div className="flex flex-col  h-[calc(50vh)]">
-      <div className="border-b shadow-md">
-        <div className="container py-4 flex items-center justify-between mx-auto">
-          <div></div>
-          <div className="flex space-x-5">
-            {steps.map((step, index) => (
-              <div className="flex items-center space-x-3" key={index}>
-                <div>
-                  {step.number < currentStep && (
-                    <AiFillCheckCircle size={24} color="#3BC592" />
-                  )}
-                  {step.number == currentStep && (
-                    <div className="w-6 aspect-square text-sm bg-primary text-white rounded-full font-bold flex items-center justify-around">
-                      {step.number}
-                    </div>
-                  )}
-                  {step.number > currentStep && (
-                    <div className="w-6 aspect-square text-sm bg-transparent text-base-content border rounded-full font-bold flex items-center justify-around">
-                      {step.number}
-                    </div>
-                  )}
-                </div>
-                <div className="text-lg">{step.text}</div>
-                {index != steps.length - 1 && (
-                  <div>
-                    <IoIosArrowForward />
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
+    <div className="flex items-center justify-start p-4">
+      <div className="flex h-[calc(100vh_-_40px)] w-full gap-x-2">
+        <div className="bg-gradient-to-b flex flex-col flex-shrink-0 from-default-100 gap-y-8 h-full hidden items-start lg:flex px-8 py-6 rounded-large shadow-small to-secondary-100 via-danger-100 w-[344px]">
           <div>
             <a href="/sign-up/">
               <AiOutlineClose />
             </a>
           </div>
+          <div>
+            <div></div>
+          </div>
+
+          {steps.map((step, index) => (
+            <div className="flex space-x-3" key={index}>
+              <div>
+                {step.number < currentStep && (
+                  <AiFillCheckCircle size={24} color="#3BC592" />
+                )}
+                {step.number == currentStep && (
+                  <div className="w-6 aspect-square text-sm bg-primary text-white rounded-full font-bold flex items-center justify-around">
+                    {step.number}
+                  </div>
+                )}
+                {step.number > currentStep && (
+                  <div className="w-6 aspect-square text-sm bg-transparent text-base-content border rounded-full font-bold flex items-center justify-around">
+                    {step.number}
+                  </div>
+                )}
+              </div>
+              <div className="text-lg">{step.text}</div>
+              {index != steps.length - 1}
+            </div>
+          ))}
         </div>
       </div>
-      <div className="flex-1 relative pt-10">
+      <div className="flex-1 relative">
         <CurrentForm />
       </div>
     </div>
