23fa2060501889a353513da9c7174e00e8cc40fb
Shahzod <shahzod.salim95@gmail.com>
2024-06-24 20:48:50 +0500

    asd

--- frontend_next/app/news/[id]/page.tsx
+++ frontend_next/app/news/[id]/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+import { apiClient } from "@frontend_next/lib/eden";
+import { Button } from "@nextui-org/button";
+import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Image } from "@nextui-org/react";
+
 export default function NewsDetailPage({
   params: { id },
 }: {
@@ -5,6 +14,42 @@ export default function NewsDetailPage({
     id: string;
   };
 }) {
-  console.log(id);
-  return <div>NewsDetailPage</div>;
+  const router = useRouter();
+  const { data: news } = useQuery({
+    queryKey: ["news"],
+    queryFn: async () => {
+      const { data } = await apiClient.api.news.public.get({
+        query: {
+          limit: "20",
+        },
+      });
+      return data;
+    },
+  });
+
+  if (!news) {
+    return <></>;
+  }
+  const newsItem = news.find((item: any) => item.id == id);
+  console.log(newsItem);
+  return (
+    <div>
+      <Button onPress={() => router.back()}> Back</Button>
+
+      <div className="flex justify-end pb-4 w-full">
+        <small className="text-default-200 dark:text-default-800 ">
+          <div>{dayjs(newsItem.created_at).format("DD MMM YYYY")}</div>
+        </small>
+      </div>
+      <h1>{newsItem.title}</h1>
+      <Image
+        removeWrapper
+        alt="Card background"
+        width="100%"
+        className="z-0 w-full h-full object-cover"
+        src={newsItem.image}
+      />
+      <div>{newsItem.description}</div>
+    </div>
+  );
 }
